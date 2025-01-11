a8160c5691f4f8dcb895e87859544f42532d1e1a
Sadaquat Ali <Sadaquat2305@gmail.com>
2025-01-11 10:34:33 +0530

    Image file is uploaded on cloudinary

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,26 @@
-import { Router } from "express"
+import { Router } from "express";
+import { 
+   
+    registerUser, 
+   
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
-import {registerUser} from "../controllers/user.controller.js"
 
 const router = Router()
 
-router.route("/register").get(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
 export default router
